a70a48c9cbc7872bec261031c3fa172f4daeb90c
Tanay Khale <91783433+tanaykhale@users.noreply.github.com>
2025-05-16 22:54:20 +0530

    'newServer'

---
+++ Server/index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const mysql = require('mysql2')
+const cors = require('cors')
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Arrow1@123',
+    database: 'knowitdb',
+})
+
+con.connect((err) => {
+    if (!err) console.log("Connected to SQL.....");
+    else console.log('Failed..');
+})
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.listen(9000, () => {
+    console.log("Server started!!!");
+})
+
+app.get('/emp/:no', (req, res) => {
+    let query = 'select * from emp where deptno=' + req.params.no;
+    con.query(query, (err, data) => {
+        if (!err)
+            res.send(data);
+    })
+})
+app.get('/dept', (req, res) => {
+    con.query('select * from dept', (err, data) => {
+        if (!err)
+            res.send(data);
+    })
+})
+
+app.get('/cust', (req, res) => {
+
+    con.query('select * from customer', (err, data) => {
+        if (!err) {
+            res.setHeader('content-type', 'application/json')
+            res.send(data);
+        }
+    })
+})
+app.post('/cust', (req, res) => {
+
+    let id = req.body.id;
+    let name = req.body.name;
+    let loc = req.body.loc;
+    console.log(id + '  ' + name + '  ' + loc);
+    let query = 'insert into customer values(?,?,?)';
+    con.query(query, [id, name, loc], (err, data) => {
+        if (!err)
+            console.log('data inserted ....');
+        res.send({ "msg": "data inserted" })
+    })
+})
+app.delete('/cust/:val', (req, res) => {
+    let val = req.params.val;
+    console.log(val);
+    let query = `delete from customer where custid=${val}`;
+    con.query(query, (err, data) => {
+        if (!err) {
+            console.log('Data deleted !!!')
+            res.send({ "msg": "data deleted" })
+        }
+    })
+})
+
